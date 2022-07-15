6092e497a8a8e24fce7a8f5addd69ddde22aab3a
restudo <restudooctavriandi@gmail.com>
2022-07-15 20:55:37 +0700

    experimen recoil
    - ya eksperimen

---
+++ Assets/Script/Orions/weapon/firePos.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firePos : MonoBehaviour
+{
+ [SerializeField] float rotateSpeed;
+
+    void Update(){
+        transform.localEulerAngles = new Vector3(0, 0, Mathf.PingPong(Time.time * rotateSpeed, 8) - 4);
+    }
+}
